e79c4b3a291da92a72bad5e519e16ae5fd3284f4
Giorgi <129960752+beberi1@users.noreply.github.com>
2023-11-10 13:40:36 +0400

    Add files via upload
    + შესაძლებლობა გადმოიწერო .zip ფაილი

--- js.js
+++ js.js
@@ -1,11 +1,22 @@
 function gadmowera() {
-  var fileUrl = 'fotoebi/texti.txt';
+
+  // .zip faili unda iyos fotoebi/ragaca.zip
+  var downloadLink = document.createElement('a');
+  downloadLink.href = 'fotoebi/ragaca.zip';
+  downloadLink.download = 'ragaca.zip';
+  downloadLink.click();
 
 
-  var anchor = document.createElement('a');
-  anchor.href = fileUrl;
-  anchor.download = 'texti.txt';
-  document.body.appendChild(anchor);
-  anchor.click();
-  document.body.removeChild(anchor);
+//#################################################################
+
+  // .txt faili unda iyos fotoebi/texti.txt
+
+  // var fileUrl = 'fotoebi/texti.txt';
+
+  // var anchor = document.createElement('a');
+  // anchor.href = fileUrl;
+  // anchor.download = 'ragaca';
+  // document.body.appendChild(anchor);
+  // anchor.click();
+  // document.body.removeChild(anchor);
 }
